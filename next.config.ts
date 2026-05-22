deed4e64e9ee9f870e46bfc5670ae88edd3ece64
Muhammad Oka <oka@muhammadoka.dev>
2026-05-22 10:35:41 +1000

    increase max body size limit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone'
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
